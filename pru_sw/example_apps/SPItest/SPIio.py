55001dcbab9c2e1abd46da855c59be3384afcd3b
Terrence McGuckin <terrence@ephemeron-labs.com>
2013-06-10 11:59:53 -0400

    added SPIio.py

---
+++ pru_sw/example_apps/SPItest/SPIio.py
@@ -0,0 +1,104 @@
+
+from mmap import mmap
+import time, struct
+
+MCSPI1_offset = 0x481a0000
+MCSPI1_size = 0xfff
+
+CM_PER = 0x44E00000
+
+CM_PER_SPI1_CLK_CTRL =  0x50 | CM_PER
+
+
+
+#TODO: put inside a dictionary or make a object for these offsets 
+MCSPI_REVISION      = 0x000
+MCSPI_SYSCONFIG     = 0x110
+MCSPI_SYSSTATUS     = 0x114
+MCSPI_IRQSTATUS     = 0x118
+MCSPI_IRQENABLE     = 0x11c
+MCSPI_WAKEUPENABLE  = 0x120
+MCSPI_SYST          = 0x124
+MCSPI_MODULCTRL     = 0x128
+
+MCSPI_XFERLEVEL     = 0x17c
+MCSPI_DAFTX         = 0x180   # DMA address aligned FIFO TX register
+MCSPI_DAFRX         = 0x1a0   # DMA address aligned FIFO RX register
+
+MCSPI_CH0CONF       = 0x12c
+MCSPI_CH0STAT       = 0x130
+MCSPI_CH0CTRL       = 0x134
+MCSPI_TX0           = 0x138
+MCSPI_RX0           = 0x13c
+
+MCSPI_CH1CONF       = 0x140
+MCSPI_CH1STAT       = 0x144
+MCSPI_CH1CTRL       = 0x148
+MCSPI_TX1           = 0x14c
+MCSPI_RX1           = 0x150
+
+# This is where we can change the register configuartions values and put them all together
+
+# sysconfig register setup
+CLOCKACTIVITY       = 0x3 << 8  # 0x3 ocp and functional clocks maintained
+SIDLEMODE           = 0x1 << 3      # 0x1 idle request ignored
+AUTOIDLE            = 0x0               # 0x1 automatic ocp clock strategy is applied
+
+SYSCONFIG = 0x0000| CLOCKACTIVITY | SIDLEMODE | AUTOIDLE
+
+
+EOWKE                   = 0x0 << 17
+RX0_OVERFLOWENABLE      = 0x0 << 3
+RX0_FULL_ENABLE         = 0x0 << 3
+TX0_UNDERFLOW_ENABLE    = 0x0 << 1
+TX0_EMPTY_ENABLE        = 0x0
+
+IRQENABLE = 0x0000 | EOWKE | RX0_OVERFLOWENABLE | RX0_FULL_ENABLE | TX0_UNDERFLOW_ENABLE | TX0_EMPTY_ENABLE
+
+# SSB  ?
+SPIENDIR                = 0x0 << 10     # output in master mode
+SPIDATDIR1              = 0x0 << 9      # 0x0 output
+SPIDATDIR0              = 0x1 << 8      # 0x1 input
+SPICLK                  = 0x0 << 6      # driven low
+SPIDAT_1                = 0x0 << 5
+SPIDAT_0                = 0x0 << 4      # driven low if output
+SPIEN_1                 = 0x0 << 1      # driven low
+SPIEN_0                 = 0x0             # driven low
+SYSTEMREG = 0x0000 | SPIENDIR | SPIDATDIR1 | SPIDATDIR0 | SPICLK | SPIDAT_1 |SPIDAT_0 | SPIEN_1 | SPIEN_0
+
+
+FDAA                   = 0x0 << 8      # 0x0 FIFO manged by CSPI_tx and rx registers
+MOA                    = 0x0 << 7      # multiple word access disabled
+INITDLY                = 0x0 << 4      # no intial delay
+SYSTEM_TEST            = 0x0 << 3      # Functional mode
+MS                     = 0x0 << 2      # This module is a master
+PIN34                  = 0x0 << 1      # 0x0 SPIEN is used as chip select 
+SINGLE                 = 0x0             # 0x0 more than one channel will be used in master mode
+MODCONTROL = 0x0000 | FDAA| MOA | INITDLY | SYSTEM_TEST | MS | PIN34 | SINGLE
+
+
+
+CLKG                   = (0x0 << 29)       # 0x0 clock divider granularity power of 2
+FFER                   = (0x0 << 28)       # FIFO enabled for recieve, 0x0 not used
+FFEW                   = (0x0 << 27)       # FIFO enabled for transmit, 0x0 not used
+TCS                    = (0x0 << 25)       # 0.5 clock cycle delay 
+SBPOL                  = (0x0 << 24)       #start bit held to zero
+SBE                    = (0x0 << 23)       # start bit enable  , 0x0 default set by WL
+SPIENSLV               = (0x0 << 21)       # spi select signal detection on ch 0
+FORCE                  = (0x0 << 20)       # manual assertion to keep SPIEN active between SPI words
+TURBO                  = (0x0 << 19)       # 0x0 turbo is deactivated 
+IS                     = (0x1 << 18)       # Input select SPIDAT1 selected for reception
+DPE1                   = (0x1 << 17)       # 0x1 no Transmission enable for data line 1
+DPE0                   = (0x0 << 16)       # data line zero selected for transmission
+DMAR                   = (0x0 << 15)       # DMA read request is disabled
+DMAW                   = (0x0 << 14)       # DMA write request is disabled
+TRM                    = (0x2 << 12)       #Transmit only   
+WL                     = (0x1f << 7)        #word length 4 bits long
+EPOL                   = (0x0 << 6)        # spien is held low during active state
+CLKD                   = (0x1 << 2)        # Clk frequency divider
+POL                    = (0x0 << 1)        # SPI clock is held high during ative state
+PHA                    = (0x1)             # data latched on odd numbered edges of SPICLK
+CH_CONF = 0x0000 | CLKG | FFER | FFEW | TCS | SBPOL | SBE | SPIENSLV
+    | FORCE | TURBO | IS | DPE1 | DPE0 | DMAR | DMAW | TRM | WL | EPOL | CLKD | POL | PHA
+
+  
